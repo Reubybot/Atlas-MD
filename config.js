d4aa6bfaca3efe954d569aec7498cc97793369e8
Reubybot <127138141+Reubybot@users.noreply.github.com>
2023-03-06 18:56:42 +0100

    Update config.js

--- config.js
+++ config.js
@@ -32,7 +32,7 @@
 require("dotenv").config();
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "918101187835";   // You can Change this number //
+  gg = "2347031350146";   // You can Change this number //
 }
 
 // -------------------------------------------------------------- //
@@ -44,8 +44,8 @@ global.sessionId = process.env.SESSION_ID || "ok";
 global.prefa = process.env.PREFIX || "-";
 global.tenorApiKey =
   process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
-global.packname = process.env.PACKNAME || `Atlas MD`;
-global.author = process.env.AUTHOR || "by: Team Atlas";
+global.packname = process.env.PACKNAME || `REUBY.MD`;
+global.author = process.env.AUTHOR || "by: DR REUBY";
 global.port = process.env.PORT || "8000";
 
 module.exports = {
@@ -55,19 +55,19 @@ module.exports = {
 // ---------------------Do Not Modify this part------------------- //
 
 global.mess = {
-  jobdone: "Job done...",
-  useradmin: "Sorry, only *Group Admins* can use this command *Baka*!",
+  jobdone: "Job done by DR REUBY...",
+  useradmin: "DR REUBY warns!, only *Group Admins* can use this command *Baka*!",
   botadmin:
-    "Sorry, i cant execute this command without being an *Admin* of this group.",
-  botowner: "Only my *Owner* can use this command, Baka!",
+    "Sorry, DR REUBY execute this command without being an *Admin* of this group.",
+  botowner: "Only *DR REUBY* can use this command, Baka!",
   grouponly: "This command is only made for *Groups*, Baka!",
   privateonly: "This command is only made for *Private Chat*, Baka!",
   botonly: "Only the *Bot itself* can use this command!",
-  waiting: "Chotto Matte...",
-  nolink: "Please provide me *link*, Baka!",
-  error: "An error occurd!",
+  waiting: "PLEASE WAIT DR REUBY IS PROCESSING...",
+  nolink: "Please provide DR REUBY *link*, Baka!",
+  error: "An error occured!",
   banned: `You are *Banned* fron using commands!  \n\nType *${prefa}owner* or *${prefa}support* to submit a request to unban yourself !`,
   bangc: "This Group is *Banned* from using Commands!",
-  nonsfw: "Dont be a pervert Baka! This is not a NSFW enabled group!",
+  nonsfw: "DR REUBY hates perverts Baka! This is not a NSFW enabled group!",
 };
 
